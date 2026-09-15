d890e360bd28d16f771701d64c0db0540fb9b4aa
Prashant Singh <prashantsingh65820@gmail.com>
2026-09-15 22:22:10 +0530

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,11 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "Prashant";
+        int age = 20;
+        String college = "COER University";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("College: " + college);
+    }
+}
